44b649063bccd6a36dd191d3a1312ba612e813d7
Marina Gonçalves <marinafgoncalves1995@gmail.com>
2024-02-04 18:05:14 +0000

    add some basic examples;

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,31 @@
 "use strict";
+const personOne = { name: 'Cat', age: 22 };
+var people = [
+    { name: 'Ana', age: 21 },
+    { name: 'Diogo', age: 32 },
+    { name: 'Nathan', age: 28 },
+    personOne
+];
 function printPersonInfo(person) {
     console.log(`${person.name} is ${person.age} years old.`);
 }
 printPersonInfo({ name: 'Ana', age: 21 });
+function sumAges(people) {
+    return people.reduce((sum, person) => sum + person.age, 0);
+}
+console.log(sumAges(people));
+let things = [1, true, "wrong"];
+var addresses = [
+    ['Porto', 'Rua do Cubo', 12],
+    ['Braga', 'Largo da Fonte', 22],
+    ['Guimarães', 'Rua da PEdra', 321]
+];
+function getAddress(addresses) {
+    const randomIndex = Math.floor(Math.random() * addresses.length);
+    return addresses[randomIndex];
+}
+const randomAddress = getAddress(addresses);
+console.log(randomAddress);
+// union types
+let someId;
+//13
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,48 @@ interface Person{
     age : number;
     
 }
-function printPersonInfo(person: Person){
+const personOne: Person ={name:'Cat', age:22};
+var people: Person[] = [
+    {name:'Ana', age: 21},
+    {name : 'Diogo', age: 32},
+    {name:'Nathan', age: 28},
+    personOne
+]; 
+
+function printPersonInfo(person: Person):void{
     console.log(`${person.name} is ${person.age} years old.`);
 }
 printPersonInfo({name:'Ana',age: 21});
+
+
+function sumAges(people : Person[]): number{
+    return people.reduce((sum, person) => sum + person.age, 0);
+}
+
+console.log(sumAges(people));
+let things = [1, true, "wrong"];
+
+// type aliases
+type Address = [string, string, number];
+var addresses: [Address, Address, Address] =[
+    ['Porto', 'Rua do Cubo', 12],
+    ['Braga', 'Largo da Fonte', 22],
+    ['Guimarães', 'Rua da PEdra', 321]
+];
+function getAddress(addresses:Address[]): Address{
+    const randomIndex = Math.floor(Math.random()*addresses.length);
+
+    return addresses[randomIndex];
+}
+
+const randomAddress: Address = getAddress(addresses);
+console.log(randomAddress);
+
+
+// union types
+
+let someId: number|null;
+type Id = number|string;
+
+
+//13
